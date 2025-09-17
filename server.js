ea4b2bb694ce58144af357ec2ac97bc1895e11d8
Krishna Bansal <83119689+bansalKrishna311@users.noreply.github.com>
2025-09-17 15:54:43 +0530

    dfd

--- server.js
+++ server.js
@@ -1,32 +1,28 @@
 const app = require("./src/app");
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
+// const mongoose = require("mongoose");
+// const dotenv = require("dotenv");
 const { SecretsManagerClient, GetSecretValueCommand } = require("@aws-sdk/client-secrets-manager");
 
-dotenv.config(); // load .env if available
+// dotenv.config(); // load .env if available
 
-async function getDbUri() {
-  if (process.env.MONGO_URI) {
-    // local dev
-    //console.log("Using local .env MONGO_URI" + process.env.MONGO_URI );
-    return process.env.MONGO_URI;
-  } else {
-    // production (App Runner)
-      const client = new SecretsManagerClient({ region: "ap-south-1" });
-    const command = new GetSecretValueCommand({ SecretId: "mongoDbSecret" });
-    const response = await client.send(command);
-    return JSON.parse(response.SecretString).MONGO_URI;
-  }
-}
-// heeh
-app.get("/", (req, res) => {
-  res.send(" API is running");
-});
+// async function getDbUri() {
+//   if (process.env.MONGO_URI) {
+//     // local dev
+//     //console.log("Using local .env MONGO_URI" + process.env.MONGO_URI );
+//     return process.env.MONGO_URI;
+//   } else {
+//     // production (App Runner)
+//       const client = new SecretsManagerClient({ region: "ap-south-1" });
+//     const command = new GetSecretValueCommand({ SecretId: "mongoDbSecret" });
+//     const response = await client.send(command);
+//     return JSON.parse(response.SecretString).MONGO_URI;
+//   }
+// }
 
 (async () => {  
   try {
-    const mongoUri = await getDbUri();
-    await mongoose.connect(mongoUri);
+    // const mongoUri = await getDbUri();
+    // await mongoose.connect(mongoUri);
     console.log("✅ MongoDB connected");
 
     const PORT = process.env.PORT || 3000;
@@ -35,3 +31,9 @@ app.get("/", (req, res) => {
     console.error("❌ Failed to start server", err);
   }
 })();
+
+
+
+app.get("/", (req, res) => {
+  res.send(" API is running");
+});
